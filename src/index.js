892287ad1a4db55ec81e8f131832e3ddd5a6a3b4
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-08-09 09:06:33 +0630

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const fs = require("fs");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.get("/video", (req, res) => {
-    const path =  "./video/Raymond.mp4";
-    fs.stat(path, (err, stats) => {
+    const videoPath = path.join("./video","Raymond.mp4");
+    fs.stat(videoPath, (err, stats) => {
         if (err) {
             console.error("An error occurred");
             res.sendStatus(500);
@@ -15,9 +16,10 @@ app.get("/video", (req, res) => {
             "Content-Length": stats.size,
             "Content-Type": "video/mp4",
         });
-        fs.createReadStream(path).pipe(res);
+        fs.createReadStream(videoPath).pipe(res);
     });
 });
 app.listen(PORT, () => {
     console.log(`Video Streaming  app listening on port ${PORT}!`);
 });
+
