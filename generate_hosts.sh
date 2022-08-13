da26de9d57ac60c94c8839968a12142630ca9efd
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-08-13 20:27:49 +0630

    Update generate_hosts.sh

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -2,7 +2,7 @@
 
 # Update /etc/hosts file
 sudo bash -c 'cat >> /etc/hosts' << EOF
-192.168.101.10	k8s_master.ayalab.local    k8s_master
-192.168.101.20	k8s_worker01.ayalab.local  k8s_worker01
-192.168.101.30	k8s_worker02.ayalab.local  k8s_worker02
+192.168.101.10	k8smaster.ayalab.local    k8smaster
+192.168.101.20	k8sworker01.ayalab.local  k8sworker01
+192.168.101.30	k8sworker02.ayalab.local  k8sworker02
 EOF
