9055d3cbee7177af29c9751469cac02ffac8180d
kosoewin <53328736+kosoewin@users.noreply.github.com>
2023-01-23 12:40:02 +0630

    Update setup_k8s.sh

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -34,5 +34,5 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt update -y
+sudo apt install -y kubelet kubeadm kubectl
